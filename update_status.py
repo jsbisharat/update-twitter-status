b4af122673f5eba2d4538a5efe69cfe95dce3165
Joseph Bisharat <ybisharat13@yahoo.com>
2018-02-09 00:21:30 -0800

    Update update_status.py

--- update_status.py
+++ update_status.py
@@ -1,4 +1,5 @@
 # The purpose of this function is to simply update the user's profile with a basic status
 import credentials
 
+# Insert whatever status you want in between the quotation marks
 credentials.api.update_status('Hello World!')
